42b2efa3f8d1419f9077f4257d0542b3567714e5
nikitosbarbados3-prog <nikitosbarbados3@gmail.com>
2026-04-30 18:35:34 +0300

    Update ClassHomework.kt

--- app/src/main/java/com/example/kotlinclasshomework/kotlinClass/ClassHomework.kt
+++ app/src/main/java/com/example/kotlinclasshomework/kotlinClass/ClassHomework.kt
@@ -9,5 +9,7 @@ package com.example.kotlinclasshomework.kotlinClass
  * Посмотри, что будет выведено в терминал
  */
 fun main() {
-    // TODO здесь нужно создать объект своего класса и вызывать у него методы haveLunch() и goHome()
+    val technicalWriter = TechnicalWriter()
+    technicalWriter.haveLunch()
+    technicalWriter.goHome()
 }
